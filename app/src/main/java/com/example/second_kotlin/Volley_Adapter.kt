d994aae52b2fb56081f27f1bdad59109cceb0d9e
devimumo <43302770+devimumo@users.noreply.github.com>
2019-10-22 11:13:17 +0300

    Applied array list data to the recycler view

--- app/src/main/java/com/example/second_kotlin/Volley_Adapter.kt
+++ app/src/main/java/com/example/second_kotlin/Volley_Adapter.kt
@@ -1,25 +1,38 @@
 package com.example.second_kotlin
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.custom_layout.view.*
+import kotlinx.android.synthetic.main.video_row.view.*
 
 class Volley_Adapter (val user_list:ArrayList<User>): RecyclerView.Adapter<Volley_Adapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+val view=LayoutInflater.from(parent?.context).inflate(R.layout.custom_layout,parent,false)
+    return  ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+return  user_list.size   }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+val user_data: User=user_list[position]
+
+    holder?.itemview.name.text=user_data.name
+
+        holder?.itemview.jina.text=user_data.jina
+
     }
 
 
     class  ViewHolder(val itemview: View): RecyclerView.ViewHolder(itemview)
-    {}
+    {
+
+
+        val name_text=itemview.findViewById<View>(R.id.name)
+        val jina_text=itemview.findViewById<View>(R.id.jina)
+    }
 
 
 }
--- app/src/main/java/com/example/second_kotlin/Volley_recycler.kt
+++ app/src/main/java/com/example/second_kotlin/Volley_recycler.kt
@@ -2,11 +2,64 @@ package com.example.second_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_volley_recycler.*
 
 class Volley_recycler : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_volley_recycler)
+
+
+recycler_view.layoutManager=LinearLayoutManager(this)
+
+
+
+        val users_list=ArrayList<User>()
+
+        users_list.add(User("davy","machakos"))
+        users_list.add(User("Faith","Kithimani"))
+        users_list.add(User("Mary","Nairobi"))
+        users_list.add(User("Justo","JKitengela"))
+        users_list.add(User("Daniel","Eldoret"))
+        users_list.add(User("Jeffry","Matuu"))
+        users_list.add(User("Antony","Mombasa"))
+        users_list.add(User("Libi","Kisumu"))
+
+        users_list.add(User("davy","machakos"))
+        users_list.add(User("Faith","Kithimani"))
+        users_list.add(User("Mary","Nairobi"))
+        users_list.add(User("Justo","JKitengela"))
+        users_list.add(User("Daniel","Eldoret"))
+        users_list.add(User("Jeffry","Matuu"))
+        users_list.add(User("Antony","Mombasa"))
+        users_list.add(User("Libi","Kisumu"))
+
+
+        users_list.add(User("davy","machakos"))
+        users_list.add(User("Faith","Kithimani"))
+        users_list.add(User("Mary","Nairobi"))
+        users_list.add(User("Justo","JKitengela"))
+        users_list.add(User("Daniel","Eldoret"))
+        users_list.add(User("Jeffry","Matuu"))
+        users_list.add(User("Antony","Mombasa"))
+        users_list.add(User("Libi","Kisumu"))
+
+
+        users_list.add(User("davy","machakos"))
+        users_list.add(User("Faith","Kithimani"))
+        users_list.add(User("Mary","Nairobi"))
+        users_list.add(User("Justo","JKitengela"))
+        users_list.add(User("Daniel","Eldoret"))
+        users_list.add(User("Jeffry","Matuu"))
+        users_list.add(User("Antony","Mombasa"))
+        users_list.add(User("Libi","Kisumu"))
+
+        val adap=Volley_Adapter(users_list)
+        recycler_view.adapter=adap
+
     }
 }
